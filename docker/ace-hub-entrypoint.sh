ff171fed9d5d3c54db80ba78d9e557053200f397
Brian Stieber <bstieber@toyon.com>
2020-09-09 19:47:42 +0000

    Update ace-hub-entrypoint.sh

--- docker/ace-hub-entrypoint.sh
+++ docker/ace-hub-entrypoint.sh
@@ -7,6 +7,11 @@ id
 echo "Changing to $FIXUID:$FIXGID"
 gosu $FIXUID:$FIXGID "$@"
 
+cd /home/coder/aerospacerl
+git remote set-url origin https://$ACE_USER:$GIT_PASSWORD@git.act3-ace.com/kyle.dunlap/aerospacerl.git
+git pull
+git checkout master
+
 # or we can create the actual user by name
 # adduser --uid $FIXUID --gid $FIGXGID $FIXUSER 
 # gosu $FIXUSER "$@"
