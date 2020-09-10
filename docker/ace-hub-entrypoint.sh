c54c5a7e5aade59e5d7fd41fe087a60a63119b45
Brian Stieber <bstieber@toyon.com>
2020-09-10 08:25:51 -0700

    move git user change befor fixuid

--- docker/ace-hub-entrypoint.sh
+++ docker/ace-hub-entrypoint.sh
@@ -1,17 +1,13 @@
 #!/usr/bin/env bash
 
-# cd $HOME
-
-echo "Originally running as"
-id
-echo "Changing to $FIXUID:$FIXGID"
-gosu $FIXUID:$FIXGID "$@"
+Log="/home/coder/log.txt"
 
 cd /home/coder/aerospacerl
 git remote set-url origin https://$ACE_USER:$GIT_PASSWORD@git.act3-ace.com/kyle.dunlap/aerospacerl.git
 git pull
 git checkout master
 
-# or we can create the actual user by name
-# adduser --uid $FIXUID --gid $FIGXGID $FIXUSER 
-# gosu $FIXUSER "$@"
+echo "Originally running as" > $Log
+id
+echo "Changing to $FIXUID:$FIXGID" >> $Log 
+gosu $FIXUID:$FIXGID "$@"
